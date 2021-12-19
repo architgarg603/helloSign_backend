eb95c70993c5c696d14d5696cdffec12a433377b
Architgarg603 <architgarg603@gmail.com>
2021-12-19 22:53:10 +0530

    change

--- public/js/call.js
+++ public/js/call.js
@@ -58,7 +58,7 @@ $("#join-form").submit(async function (e) {
 });
 $("#leave").click(function (e) {
   leave();
-  window.location.href = "";
+  window.location.href = "https://hellosign.netlify.app/leaderboard";
 });
 async function join() {
   // Add an event listener to play remote tracks when remote user publishes.
